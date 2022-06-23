c028efe59bb62e22681249262641d2668d690cbb
ordinaryguy <69292339+ordinaryguy@users.noreply.github.com>
2022-06-24 02:59:33 +0800

    Update google-map.js
    Changing lat and long

--- js/google-map.js
+++ js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(12.519004803841897, 123.06890076598836);
     // 39.399872
     // -8.224454
     
